f8ad84027bd2e821f29cab6df23959bd3b66c319
Abdurahim2005 <143182268+Abdurahim2005@users.noreply.github.com>
2024-12-31 15:49:55 +0500

    instagram.py

---
+++ instagram.py
@@ -0,0 +1,20 @@
+import os
+from yt_dlp import YoutubeDL
+
+# Instagram videolarini yuklab olish funksiyasi
+def download_instagram_video(url: str) -> str:
+    ydl_opts = {
+        'format': 'mp4',
+        'outtmpl': 'instagram_video.mp4',  # Yuklab olingan fayl nomi
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        try:
+            info = ydl.extract_info(url, download=True)
+            return "instagram_video.mp4"
+        except Exception as e:
+            if "private" in str(e).lower():
+                raise ValueError(
+                    "❗️Bu video yopiq akkauntga tegishli bo'lishi mumkin.\n😕Hozirga bu videoni yuklab olish imkoni yo'q.\n👨‍💻Adminlar bu muammo ustida ishlashmoqda!"
+                )
+            else:
+                raise e
